562eab71b8d2f970f47618f665fd567eea5c6b34
liu1490911423 <1490911423@qq.com>
2020-03-18 17:06:13 +0800

    v1.0

---
+++ lib/ESign.php
@@ -0,0 +1,359 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sage
+ * Date: 2018/9/26
+ * Time: 14:59
+ */
+
+namespace Sign;
+
+
+/**
+ * 调用接口
+ * Class Login
+ * @package Common\Logic
+ */
+
+//$info = array(
+//    "thirdPartyUserId"=>"2019082630134",
+//    "name"=>"lhx",
+//    "idType"=>"CRED_PSN_CH_IDCARD",
+//    "idNumber"=>"371204199604234930",
+//    "mobile"=>"18357465647",
+//    "email"=>"wuhuan@tsign.com"
+//);
+//Logic('ESign')->createByThirdPartyUserId($info);
+class Esign
+{
+    private $config;
+    private $until;
+
+    public $url ;
+
+    public $Link_URL=[
+        1=>'/v1/oauth2/access_token?',//token获取
+        2=>'/v1/accounts/createByThirdPartyUserId',//个人账户创建
+        3=>'/v1/organizations/createByThirdPartyUserId',//企业账户创建
+        4=>'/v1/files/getUploadUrl',//上传方式创建文件
+        5=>'/v1/signflows',//创建签署流程
+        6=>'/v1/signflows/{flowId}/documents',//添加流程文件’；
+        7=>'/v1/signflows/{flowId}/signfields/platformSign',//添加平台自动盖章签署区
+        8=>'/v1/signflows/{flowId}/signfields/handSign',//添加手动盖章签署区
+        9=>'/v1/signflows/{flowId}/start',//签署流程开启
+        10=>'/v1/signflows/{flowId}/archive',//签署流程归档
+        11=>'/v1/signflows/{flowId}/documents',//流程文档下载
+        12=>'/v1/signflows/{flowId}/executeUrl'//获取签署地址
+    ];
+
+    function __construct() {
+
+        $this->until  = new UntilService();
+
+        $this->config=[
+            'appId'=>'4438793861',
+            'secret'=>'da3fcada0c7e5cd7eed5feb2b03b545d',
+        ];
+
+        $this->url  = 'https://smlopenapi.esign.cn';
+    }
+
+    /**
+     * post请求参数处理
+     * @param $data
+     * @return string
+     */
+
+    function fixData($data){
+        $o = "";
+        foreach ( $data as $k => $v )
+        {
+            $o.= "$k=" . urlencode( $v ). "&" ;
+        }
+        $post_data = substr($o,0,-1);
+        return $post_data;
+    }
+
+    /**
+     * 返回数据处理
+     * @param $res
+     * @return array|mixed
+     */
+    function fixReturn($res){
+
+        $res = (array)json_decode($res);
+
+        if($res['code'] == 0){
+            $data = (array)$res['data'];
+            return $data;
+
+        }else{
+            $log = "./data/logs/error_eSign".date('Y-m-d').".txt";// . time() . '.json';
+            $newL =date('Y-m-d H:i:s ',time());
+            file_put_contents($log,$newL.'-'.time().':'.'错误数据:'.json_encode($res).PHP_EOL,FILE_APPEND);
+            return $res;
+        }
+    }
+
+
+    /**
+     * @return mixed
+     * 获取token
+     */
+    function getToken(){
+
+        //$tokenCache = new SignTokenCache();
+        //$token = $tokenCache->get(1);
+        //if((!$token)) {
+            $this->config['grantType'] = 'client_credentials';
+            $data = $this->fixData($this->config);
+            $return_content = $this->until->doGet($this->url.$this->Link_URL[1].$data);
+            $result = $this->fixReturn($return_content);
+            $token = $result['token'];
+            //echo $token;
+        //}
+        return $token;
+
+    }
+
+    /**
+     * @param $info
+     * @param $num
+     * @return array|mixed
+     * @param string $method
+     * 创建账户
+     */
+    function commonRequest($info,$num,$method='post'){
+
+        $stoken = $this->getToken();
+        $data = json_encode($info);
+
+        $url = $this->url.$this->Link_URL[$num];
+        if($info['flowId']){
+            $url = $this->editUrl($url,$info['flowId']);
+        }
+
+        if($method == 'put'){
+            $return_content = $this->until->doPut($url,$this->config['appId'],$stoken);
+        }else if($method == 'get'){
+            if($info['accountId']){
+                $url = $url.'?accountId='.$info['accountId'];
+            }
+            $return_content = $this->until->doGet($url,$this->config['appId'],$stoken);
+        }else{
+            $return_content = $this->until->doPost($url,$data,$this->config['appId'],$stoken);
+        }
+        $result = $this->fixReturn($return_content);
+        return $result;
+
+    }
+
+    /**
+     * @param $info
+     * @return array|mixed
+     * 个人创建账户
+     * 返回个人ID：accountId
+     */
+    function createAccounts($info){
+        return $this->commonRequest($info,2);
+    }
+
+    /**
+     * @param $info
+     * @return array|mixed
+     * 创建企业用户
+     * 返回企业ID：orgId
+     */
+    function createOrganizations($info){
+        return $this->commonRequest($info,3);
+    }
+
+    /**
+     * @param $filePath
+     * @return string
+     * 获取contentMd5
+     */
+    function getContentBase64Md5($filePath){
+        //获取文件MD5的128位二进制数组
+        $md5file = md5_file($filePath,true);
+        //计算文件的Content-MD5
+        $contentBase64Md5 = base64_encode($md5file);
+        //echo ("contentBase64Md5=".$contentBase64Md5);
+        return $contentBase64Md5;
+    }
+
+    /**
+     * @param $filePath
+     * @param $fileName
+     * @param $fileSize
+     * @param $account
+     * @return array|mixed
+     * 上传合同
+     * 返回文件ID:fileId 和上传文件地址:uploadUrl
+     */
+    function getUploadUrl($filePath,$fileName,$fileSize,$account){
+
+        $info['contentMd5'] = $this->getContentBase64Md5($filePath);
+        $info['contentType'] = 'application/pdf';
+        $info['convert2Pdf'] = false;
+        $info['fileName'] = $fileName;
+        $info['fileSize'] = $fileSize;
+        $info['accountId'] = $account;
+
+        return $this->commonRequest($info,4);
+
+    }
+
+    /**
+     * @param $uploadUrls
+     * @param $filePath
+     * @return int|mixed|string
+     * 上传文件
+     */
+    public function  upLoadFile($uploadUrls,$filePath){
+        //文件内容
+        $fileContent = file_get_contents($filePath);
+        $contentMd5 = $this->getContentBase64Md5($filePath);
+        $status = $this->until->sendHttpPUT($uploadUrls, $contentMd5, $fileContent);
+        return $status;
+    }
+
+    /**
+     * @param $businessScene
+     * @return array|mixed
+     * 创建签署流程返回流程ID:flowId
+     */
+    function signFlows($businessScene){
+        $info = array(
+            'businessScene'=>$businessScene?$businessScene:'test',
+            'autoArchive'=>true,
+            'configInfo'=>array(
+                'noticeDeveloperUrl'=>C('sign_return_url')?C('sign_return_url'):'http://admin.cs.ttypapp.com/remedy-signOver?auth=Jxiaopi1907'
+            )
+        );
+
+        return $this->commonRequest($info,5);
+    }
+
+    /**
+     * @param $url
+     * @param $flowId
+     * @return mixed
+     * 处理请求地址
+     */
+    function editUrl($url,$flowId){
+         if(strstr($url,'{flowId}')){
+             $url = str_replace('{flowId}',$flowId,$url);
+         }
+
+         return $url;
+    }
+
+    /**
+     * @param $flowId
+     * @param $fileId
+     * @return array|mixed
+     * 流程文档添加
+     */
+    function addDocuments($flowId,$fileId){
+        $info=array(
+            'flowId'=>$flowId,
+            'docs'=>array(array('fileId'=>$fileId)));
+        return $this->commonRequest($info,6);
+    }
+
+    /**
+     * @param $flowId
+     * @param $fileId
+     * @param $sealId
+     * @return array|mixed
+     * 平台签
+     * 返回用户ID：accountId，文档ID：fileId
+     */
+    function platformSign($flowId,$fileId,$sealId){
+        $info = array(
+            'flowId'=>$flowId,
+            'signfields'=>array(array(
+                'fileId'=>$fileId,
+                'order'=>1,
+                'sealId'=>$sealId,
+                'signType'=>1,
+                'posBean'=>array(
+                    'posPage'=>'3',
+                    'posX'=>'200',
+                    'posY'=>'450',))));
+        return $this->commonRequest($info,7);
+    }
+
+    /**
+     * @param $accountId
+     * @param int $actorIndentityType
+     * @param $flowId
+     * @param $fileId
+     * @return array|mixed
+     * 手动签
+     * 返回用户ID：accountId，文档ID：fileId
+     */
+    function handSign($accountId,$flowId,$fileId){
+        $info = array(
+            'flowId'=>$flowId,
+            'signfields'=>array(array(
+                'fileId'=>$fileId,
+                'signerAccountId'=>$accountId,
+                'order'=>'1',
+                'signType'=>'1',
+                'sealType'=>'0',
+                'posBean'=>array(
+                    'posPage'=>'3',
+                    'posX'=>'460',
+                    'posY'=>'450',))));
+        return $this->commonRequest($info,8);
+    }
+
+    /**
+     * @param $flowId
+     * @return array|mixedsignFlowStart
+     * 开启签署流程
+     */
+    function signFlowStart($flowId){
+        $info['flowId'] = $flowId;
+        return $this->commonRequest($info,9,'put');
+    }
+
+    /**
+     * @param $flowId
+     * @param $accountId
+     * @return array|mixed
+     * 获取签署人签署地址
+     */
+    function  executeUrl($flowId,$accountId){
+        $info['flowId'] = $flowId;
+        $info['accountId'] = $accountId;
+        return $this->commonRequest($info,12,'get');
+    }
+
+    /**
+     * @param $flowId
+     * @return array|mixed
+     * 手动签署流程归档
+     */
+    function signFlowArchive($flowId){
+        $info['flowId'] = $flowId;
+        return $this->commonRequest($info,10,'put');
+    }
+
+    /**
+     * @param $flowId
+     * @return mixed
+     * 下载流程文档
+     * 返回 文档Id:fileId，文档名称：fileName，文档地址：fileUrl
+     */
+    function signLoad($flowId){
+        $info['flowId'] = $flowId;
+        return $this->commonRequest($info,11,'get');
+
+    }
+
+
+
+}
---
+++ lib/UntilServices.php
@@ -0,0 +1,216 @@
+<?php
+namespace Sign;
+
+
+class UntilService
+{/**
+ * @param $url
+ * @param $data
+ * @param $appID
+ * @param $appSecret
+ * @return mixed
+ */
+
+    public function doPost($url, $data, $appID, $stoken)
+    {
+        list($return_code, $return_content) = $this->http_post_data($url, $data, $appID,$stoken);
+        return $return_content;
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @param $config
+     * @param $stoken
+     * @return array
+     */
+    public function http_post_data($url, $data, $appID, $stoken) {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // 跳过检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // 跳过检查
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Tsign-Open-App-Id:".$appID, "X-Tsign-Open-Token:".$stoken, "Content-Type:application/json" ));
+        ob_start();
+        curl_exec($ch);
+        $return_content = ob_get_contents();
+        ob_end_clean();
+        $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        return array($return_code, $return_content);
+    }
+
+    //get方法
+
+    /**
+     * @param $url
+     * @param $appID
+     * @param $stoken
+     * @return mixed
+     */
+    public function doGet($url, $appID='', $stoken='')
+    {
+        list($return_code, $return_content) = $this->curl_get_https($url,$appID, $stoken);
+        return $return_content;
+    }
+
+    /**
+     * @param $url
+     * @param string $appID
+     * @param string $stoken
+     * @return array
+     */
+    public function curl_get_https($url,$appID='', $stoken='') {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // 跳过检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // 跳过检查
+
+        if($stoken){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Tsign-Open-App-Id:".$appID, "X-Tsign-Open-Token:".$stoken, "Content-Type:application/json" ));
+        }
+        ob_start();
+        curl_exec($ch);
+        $return_content = ob_get_contents();
+        ob_end_clean();
+        $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        return array($return_code, $return_content);
+    }
+
+    public function sendHttpPUT($uploadUrls, $contentMd5, $fileContent){
+        $header = array(
+            'Content-Type:application/pdf',
+            'Content-Md5:' . $contentMd5
+        );
+
+        $status = '';
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL, $uploadUrls);
+        curl_setopt($curl_handle, CURLOPT_FILETIME, true);
+        curl_setopt($curl_handle, CURLOPT_FRESH_CONNECT, false);
+        curl_setopt($curl_handle, CURLOPT_HEADER, true); // 输出HTTP头 true
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_handle, CURLOPT_TIMEOUT, 5184000);
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 120);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, false);
+
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $fileContent);
+        $result = curl_exec($curl_handle);
+        $status = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+
+        if ($result === false) {
+            $status = curl_errno($curl_handle);
+            $result = 'put file to oss - curl error :' . curl_error($curl_handle);
+        }
+        curl_close($curl_handle);
+//    $this->debug($url, $fileContent, $header, $result);
+        return $status;
+    }
+
+
+    /**
+     * @param $url
+     * @param $data
+     * @param $appID
+     * @param $stoken
+     * @return mixed
+     */
+    public function doPut($url, $appID, $stoken)
+    {
+        list($return_code, $return_content) = $this->http_put_data($url,$appID,$stoken);
+        return $return_content;
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @param $appID
+     * @param $stoken
+     * @return array
+     */
+
+
+    function http_put_data($url,$appID,$stoken){
+
+        $request_headers = array(
+            "X-HTTP-Method-Override: put",
+            "X-Tsign-Open-App-Id:".$appID,
+            "X-Tsign-Open-Token:".$stoken,
+            "Content-Type:application/json" );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);//https
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+
+        $return_content = curl_exec($ch);
+        $return_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+
+
+        curl_close($ch);
+
+        return array($return_code, $return_content);
+
+    }
+
+    function curlDownFile($file_url, $save_path = '', $file_name = '') {
+        // 没有远程url或已下载文件，返回false
+        if (trim($file_url) == '' || file_exists( $save_path.$file_name )) {
+            return false;
+        }
+
+        // 若没指定目录，则默认当前目录
+        if (trim($save_path) == '') {
+            $save_path = './';
+        }
+
+        // 若指定的目录没有，则创建
+        if (!file_exists($save_path) && !mkdir($save_path, 0777, true)) {
+            return false;
+        }
+
+        // 若没指定文件名，则自动命名
+        if (trim($file_name) == '') {
+            $file_ext = strrchr($file_url, '.');
+            $file_exts = array('.gif', '.jpg', '.png','mp3');
+            if (!in_array($file_ext, $file_exts)) {
+                return false;
+            }
+            $file_name = time() . $file_ext;
+        }
+
+        // curl下载文件
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $file_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $file = curl_exec($ch);
+        curl_close($ch);
+
+        // var_dump($file);die();
+
+        // 保存文件到指定路径
+        file_put_contents($save_path.$file_name, $file);
+
+        // 释放文件内存
+        unset($file);
+
+        // 执行成功，返回true
+        return $save_path.$file_name;
+    }
+
+
+}
